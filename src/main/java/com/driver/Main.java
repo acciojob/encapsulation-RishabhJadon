25e47767a09887ab09196f535f1c368b52aabde5
Rishabh Singh <rishabhjadon976@gmail.com>
2023-09-12 11:33:19 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+
 public class Main {
-  
+     RWOnly obj = new RWOnly();
+
+     int val = obj.setVariable(10);
+
+     //obj.variable = 10;
+     //'variable' has private access in 'com.driver.RWOnly'
+
+    int res = obj.getVariable();
+
+    //System.out.println(obj.getVariable());
+
+    //System.out.println(obj.variable);
+    //'variable' has private access in 'com.driver.RWOnly'
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int variable = 24;
+
+    public int setVariable(int variable1)
+    {
+        this.variable = variable1;
+        return variable;
+    }
+    public int getVariable()
+    {
+        return variable;
+    }
+
+
 }
